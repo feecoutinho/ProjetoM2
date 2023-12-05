0c5e2842fd5dcc039d7af0ac3551e5baaa230b5f
Fernanda Coutinho <fernandarcoutinho@gmail.com>
2023-12-04 22:48:11 -0300

    RF02 - implementa Consulta Farmacias

---
+++ src/main/java/tech/devinhouse/devinpharma/exception/FarmaciaJaCadastradaException.java
@@ -0,0 +1,6 @@
+package tech.devinhouse.devinpharma.exception;
+
+public class FarmaciaJaCadastradaException extends RuntimeException {
+    public FarmaciaJaCadastradaException(String farmacia, Long cnpj) {
+    }
+}

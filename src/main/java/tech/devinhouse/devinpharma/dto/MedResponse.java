69afa2799dfb9624ceda4d5b9a45bf8b44db5992
Fernanda Coutinho <fernandarcoutinho@gmail.com>
2023-12-05 20:59:46 -0300

    RF05 - Implementa Consulta Medicamentos

---
+++ src/main/java/tech/devinhouse/devinpharma/dto/MedResponse.java
@@ -0,0 +1,32 @@
+package tech.devinhouse.devinpharma.dto;
+
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import tech.devinhouse.devinpharma.model.TipoMedicamento;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class MedResponse {
+
+    @Id
+    private Integer nroRegistro;
+
+    private String nome;
+
+    private String laboratorio;
+
+    private String dosagem;
+
+    private String descricao;
+
+    private Float preco;
+
+    @Enumerated(EnumType.STRING)
+    private TipoMedicamento tipo;
+
+}

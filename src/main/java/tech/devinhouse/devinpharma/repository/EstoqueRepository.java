bd5d6d8d8910a9d375ca4db8745ec323140a448e
Fernanda Coutinho <fernandarcoutinho@gmail.com>
2023-12-05 22:35:59 -0300

    RF05 - Implementa Consulta Medicamentos

--- src/main/java/tech/devinhouse/devinpharma/repository/EstoqueRepository.java
+++ src/main/java/tech/devinhouse/devinpharma/repository/EstoqueRepository.java
@@ -3,8 +3,13 @@ package tech.devinhouse.devinpharma.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import tech.devinhouse.devinpharma.model.Estoque;
+import tech.devinhouse.devinpharma.model.Farmacia;
 import tech.devinhouse.devinpharma.model.IdEstoque;
+
+import java.util.List;
 
 @Repository
 public interface EstoqueRepository extends JpaRepository<Estoque, IdEstoque> {
+
+    List<Estoque> findAllByFarmacia(Farmacia farmacia);
 }
